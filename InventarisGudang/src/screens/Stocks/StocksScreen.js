3da243ec99c26d4bc1a5aeb697d3aadadfe8142d
DeboraSidabutar <103346346+DeboraSidabutar@users.noreply.github.com>
2023-07-04 11:44:11 +0700

    update

--- InventarisGudang/src/screens/Stocks/StocksScreen.js
+++ InventarisGudang/src/screens/Stocks/StocksScreen.js
@@ -1,17 +1,18 @@
 import React, { useEffect, useLayoutEffect, useState } from "react";
-import { FlatList, Text, View, TouchableHighlight, Image, ScrollView, Touchable, StatusBar } from "react-native";
+import { FlatList, Text, View, Image, TouchableHighlight, Pressable } from "react-native";
 import styles from "./styles";
 import MenuImage from "../../components/MenuImage/MenuImage";
-import { db } from "../Login/LoginScreen";
+import { getCategoryName, getRecipesByRecipeName, getRecipesByCategoryName, getRecipesByIngredientName } from "../../data/MockDataAPI";
+import { TextInput } from "react-native-gesture-handler";
 
-export default function HomeScreen(props) {
+export default function SearchScreen(props) {
   const { navigation } = props;
+
+  const [value, setValue] = useState("");
+  const [data, setData] = useState([]);
 
   useLayoutEffect(() => {
     navigation.setOptions({
-      headerTitleStyle: {
-        top: 0,
-      },
       headerLeft: () => (
         <MenuImage
           onPress={() => {
@@ -19,14 +20,57 @@ export default function HomeScreen(props) {
           }}
         />
       ),
+      headerTitle: () => (
+        <View style={styles.searchContainer}>
+          <Image style={styles.searchIcon} source={require("../../../assets/icons/search.png")} />
+          <TextInput
+            style={styles.searchInput}
+            onChangeText={handleSearch}
+            value={value}
+          />
+          <Pressable onPress={() => handleSearch("")}>
+          <Image style={styles.searchIcon} source={require("../../../assets/icons/close.png")} />
+          </Pressable>
+        </View>
+      ),
       headerRight: () => <View />,
     });
-  }, []);
+  }, [value]);
+
+  useEffect(() => {}, [value]);
+
+  const handleSearch = (text) => {
+    setValue(text);
+    var recipeArray1 = getRecipesByRecipeName(text);
+    var recipeArray2 = getRecipesByCategoryName(text);
+    var recipeArray3 = getRecipesByIngredientName(text);
+    var aux = recipeArray1.concat(recipeArray2);
+    var recipeArray = [...new Set(aux)];
+
+    if (text == "") {
+      setData([]);
+    } else {
+      setData(recipeArray);
+    }
+  };
+
+  const onPressRecipe = (item) => {
+    navigation.navigate("Recipe", { item });
+  };
+
+  const renderRecipes = ({ item }) => (
+    <TouchableHighlight underlayColor="rgba(73,182,77,0.9)" onPress={() => onPressRecipe(item)}>
+      <View style={styles.container}>
+        <Image style={styles.photo} source={{ uri: item.photo_url }} />
+        <Text style={styles.title}>{item.title}</Text>
+        <Text style={styles.category}>{getCategoryName(item.categoryId)}</Text>
+      </View>
+    </TouchableHighlight>
+  );
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <FlatList vertical showsVerticalScrollIndicator={false} numColumns={2} data={data} renderItem={renderRecipes} keyExtractor={(item) => `${item.recipeId}`} />
     </View>
   );
 }
