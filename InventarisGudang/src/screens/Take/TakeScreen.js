5102c0f8aac4c407876b0f9cffee99a29e68ba55
DeboraSidabutar <103346346+DeboraSidabutar@users.noreply.github.com>
2023-07-06 13:58:22 +0700

    update take

--- InventarisGudang/src/screens/Take/TakeScreen.js
+++ InventarisGudang/src/screens/Take/TakeScreen.js
@@ -1,11 +1,22 @@
-import React, { useEffect, useLayoutEffect, useState } from "react";
-import { FlatList, Text, View, TouchableHighlight, Image, ScrollView, Touchable, StatusBar } from "react-native";
-import styles from "./styles";
+import React, { useState, useLayoutEffect } from 'react';
+import { View, Text, TouchableOpacity, TextInput, FlatList } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import styles from './styles';
 import MenuImage from "../../components/MenuImage/MenuImage";
-import { db } from "../Login/LoginScreen";
 
-export default function HomeScreen(props) {
-  const { navigation } = props;
+const dummyBarang = [
+  { id: '1', nama: 'Barang 1' },
+  { id: '2', nama: 'Barang 2' },
+  { id: '3', nama: 'Barang 3' },
+  { id: '4', nama: 'Barang 4' },
+  { id: '5', nama: 'Barang 5' },
+];
+
+export default function AmbilScreen(props) {
+  const navigation = useNavigation();
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredBarang, setFilteredBarang] = useState(dummyBarang);
+  const [jumlahBarang, setJumlahBarang] = useState({});
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -21,12 +32,65 @@ export default function HomeScreen(props) {
       ),
       headerRight: () => <View />,
     });
-  }, []);
+  }, [navigation]);
+
+  const handleSearch = (text) => {
+    setSearchQuery(text);
+
+    const filteredData = dummyBarang.filter((barang) =>
+      barang.nama.toLowerCase().includes(text.toLowerCase())
+    );
+    setFilteredBarang(filteredData);
+  };
+
+  const handleTambah = (item) => {
+    setJumlahBarang((prevJumlah) => ({
+      ...prevJumlah,
+      [item.id]: prevJumlah[item.id] ? prevJumlah[item.id] + 1 : 1,
+    }));
+  };
+
+  const handleKurang = (item) => {
+    if (jumlahBarang[item.id] && jumlahBarang[item.id] > 0) {
+      setJumlahBarang((prevJumlah) => ({
+        ...prevJumlah,
+        [item.id]: prevJumlah[item.id] - 1,
+      }));
+    }
+  };
+
+  const renderItem = ({ item }) => (
+    <View style={styles.itemContainer}>
+      <Text style={styles.itemText}>{item.nama}</Text>
+      <TouchableOpacity style={styles.button} onPress={() => handleTambah(item)}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
+      <Text style={styles.jumlahText}>{jumlahBarang[item.id] || 0}</Text>
+      <TouchableOpacity style={styles.button} onPress={() => handleKurang(item)}>
+        <Text style={styles.buttonText}>-</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
+  const handleOke = () => {
+    console.log('Jumlah Barang:', jumlahBarang);
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <TextInput
+        style={styles.searchInput}
+        placeholder="Nama Barang"
+        value={searchQuery}
+        onChangeText={handleSearch}
+      />
+      <FlatList
+        data={filteredBarang}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
+      <TouchableOpacity style={styles.buttonOke} onPress={handleOke}>
+        <Text style={styles.buttonTextOke}>Oke</Text>
+      </TouchableOpacity>
+    </View> );
 }
--- InventarisGudang/src/screens/Take/styles.js
+++ InventarisGudang/src/screens/Take/styles.js
@@ -1,18 +1,54 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  recommendationTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginTop: 20,
-    marginLeft: 10,
-  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    padding: 20,
+  },
+  searchInput: {
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  itemContainer: {
+    flexDirection: 'row',
     alignItems: 'center',
+    marginBottom: 10,
+  },
+  itemText: {
+    flex: 1,
+  },
+  button: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    backgroundColor: '#ccc',
     justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 5,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  jumlahText: {
+    fontSize: 16,
+    marginHorizontal: 5,
+  },
+  buttonOke: {
+    backgroundColor: '#337ab7',
+    borderRadius: 5,
+    paddingVertical: 10,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonTextOke: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
 
